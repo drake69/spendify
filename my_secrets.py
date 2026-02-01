21f605ff6657adf5dca52cbee8956848264c58ef
Luigi Corsaro <lcorsaro69@gmail.com>
2026-02-01 14:13:44 +0100

    removed keys

---
+++ my_secrets.py
@@ -0,0 +1,3 @@
+OWN_NAMES = ["Luigi Corsaro", "Gerotti Elena"]
+
+API_KEY = "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"
